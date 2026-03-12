17703894fcf9d2ae2397fde085376f313756c105
Brian Rizqi <rizqibrian27@gmail.com>
2026-03-12 07:08:27 +0700

    feat: Update Vercel configuration to use `routes` and `src`/`dest` keys, simplify the install command, and add a Vercel execution header to the API entry point.

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
 
+// Check if we are running on Vercel
+if (isset($_SERVER['VERCEL_URL'])) {
+    header('X-Vercel-PHP-Executed: true');
+}
+
 // Forward Vercel requests to normal Laravel entry point
 require __DIR__ . '/../public/index.php';
